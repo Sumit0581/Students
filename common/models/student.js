eaeb77acc50e103c0486728b3045cd795cde53f0
Sumit0581 <sb03042000@gmail.com>
2019-07-02 11:03:15 +0530

    Added remote method for Marks model

--- common/models/student.js
+++ common/models/student.js
@@ -2,14 +2,15 @@
 var app = require('../../server/server');
 module.exports = function(Student) {
 
-Student.getMarks = function(id,subjectId,cb) {
+Student.getMarks = function(id,subjectId,testId,cb) {
         var Marks = app.models.Marks;
-	Marks.find({where: {studentId:id, courseId: subjectId}}, function (req,res,err) {
+	Marks.find({where: {studentId:id, courseId: subjectId, testId: testId}}, function (req,res,err) {
 	if (err) {
         	return console.log(err);
         } 
 	else {
-       		cb(null,res[0].Marks);
+       		console.log(res);
+		cb(null,res[0].Marks,res[0].testId);
         }
 	})
 }
@@ -22,9 +23,12 @@ Student.getMarks = function(id,subjectId,cb) {
 		{
 			http: {path: '/getMarks', verb: 'get'},
 			accepts:[ {arg: 'id', type: 'string', required: true},
-				  {arg: 'subjectId', type: 'string', required: true}
+				  {arg: 'subjectId', type: 'string', required: true},
+				  {arg: 'testId', type: 'string', required: true},
 				],
-			returns: {arg: 'Marks', type: 'number'}
+			returns: [{arg: 'Marks', type: 'number'},
+				  {arg: 'Test Type', type:'string'}
+				],
 		}
 	);
 
@@ -51,10 +55,59 @@ Student.getTotal = function(id,cb) {
 	);
 
 
+Student.getMarks1 = function(id,subjectId,testId,cb) {
+        var Marks = app.models.Marks;
+	Marks.find({where: {studentId:id, courseId: subjectId, testId: testId}}, function (req,res,err) {
+	if (err) {
+        	return console.log(err);
+        } 
+	else {
+       		console.log(res);
+		cb(null,res);
+        }
+	})
+}
 
 
 
 
+	Student.remoteMethod (
+		'getMarks1',
+		{
+			http: {path: '/getMarks1', verb: 'get'},
+			accepts:[ {arg: 'id', type: 'string', required: true},
+				  {arg: 'subjectId', type: 'string', required: false},
+				  {arg: 'testId', type: 'string', required: false}
+				],
+			returns: [{arg: 'result', type: 'array'}
+				],
+		}
+	);
+
+
+Student.getTotal1 = function(id,subjectId,testId,cb) {
+        var Marks = app.models.Marks;
+	Marks.find({where: {studentId:id, courseId: subjectId, testId: testId}}, function (req,res,err) {
+	var total = 0;
+	for(var i=0;i<res.length;i++){
+		total+=res[i].Marks;
+	}
+	var average = total/res.length;
+	cb(null,total,average);
+	})
+}
+
+	Student.remoteMethod (
+		'getTotal1',
+		{
+			http: {path: '/getTotal1', verb: 'get'},
+			accepts:[ {arg: 'id', type: 'string', required: true},
+				  {arg: 'subjectId', type: 'string', required: false},
+				  {arg: 'testId', type: 'string', required: false}],
+			returns: [{arg: 'Total', type: 'number'},
+				  {arg: 'Average', type: 'number'}],
+		}
+	);
 
 
 
